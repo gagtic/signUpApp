c78284f1c90ac65b17833a5e6d31963f2835aba6
gagtic <gagtic@hotmail.com>
2024-02-19 14:01:32 +0500

    resolved unused imports

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import Navigation from "./src/navigation";
-import { useFonts } from "expo-font";
 import { store } from "./src/redux/store/store";
 import { Provider } from "react-redux";
 import getFonts from "./src/utils/fonts";
